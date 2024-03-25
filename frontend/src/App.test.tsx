f8145f866b756fccdac5f4643fa1bc7baa7beeb3
avivf <avivfg8@gmail.com>
2024-03-25 09:52:53 +0200

    Another test code cleanup. TODO: tests for App.tsx and we're done

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,22 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('counter displays correct initial count', () => {
-    const { getAllByTestId } = render(<App />);
-    const countElement = getAllByTestId('count')[0];
-    const countValue = Number(countElement.textContent);
-    expect(countValue).toEqual(1);
+  it('Test', () => {
+    expect(1).toEqual(1);
   });
 });
-
-// test("2nd demo test", () =>{
-//   const mockProps = {
-//     locationPermission: false,
-//     sortByDistance: false,
-//     handleSortToggleChange: () => {},
-//     show: boolean,
-//     setShow: (toShow: boolean) => void;
-//   }
-//   render(<GeoSort />);
-//   expect(cloudSelected[0])
-// })
